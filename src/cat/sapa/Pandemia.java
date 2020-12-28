30369fc5834a4b7ed203e0129a273319043755bc
Morel-Luque <m.luque@sapalomera.cat>
2020-12-28 20:21:01 +0100

    Creació de menú i estructura switch per les opcions del menú.

--- src/cat/sapa/Pandemia.java
+++ src/cat/sapa/Pandemia.java
@@ -1,10 +1,37 @@
 package cat.sapa;
 
+import java.util.Scanner;
+
 public class Pandemia {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Prova");
-        System.out.println("Prova v2");
+        Scanner scan = new Scanner(System.in);
+        String menu = ("1. Carregar taulell\n2. Introduir malalts\n3. Transmetre virus\n4. Curar malalts\n5. Desplaçar malalts\n6. Mostrar informació\n0. Sortir");
+        System.out.println(menu);
+        //Introduir resposta
+        byte answer = scan.nextByte();
+        switch (answer) {
+            case 0:
+                System.out.println("Adeu");
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+        }
     }
 }
