5b17bebfad27b9970b0b1b78b7d477a3d8a31d64
Morel <m.luque@sapalomera.cat>
2021-01-04 20:11:49 +0100

    Finalització apartat "Transmetre virus". Començament apartat "Curar malalts". Comentaris respecte a l'apartat "Desplaçar malalts"

--- src/cat/sapa/Pandemia.java
+++ src/cat/sapa/Pandemia.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class Pandemia {
 
     public static void main(String[] args) {
-        int files = 0;
-        int columnes = 0;
         Scanner scan = new Scanner(System.in);
         String menu = ("1. Carregar taulell\n" +
                 "2. Introduir malalts\n" +
@@ -20,21 +18,24 @@ public class Pandemia {
         //Declaració variables
         int x;
         int y;
-        int[][] board = null; //Especifiquem "null" per poder utilitzar l'array a tot el programa"
+        int files = 0;
+        int columnes = 0;
+        float taxaContagi;
+        float[][] board = null; //Especifiquem "null" per poder utilitzar l'array a tot el programa"
         byte answer = scan.nextByte(); //Introduir resposta
         while (answer != 0) {
             switch (answer) {
                 case 1:
                     System.out.println("Que vols fer:\n" +
-                            "1. Crear un taulell buit(introduint les files y columnes)\n" +
-                            "2. Taulell amb malalts de forma aleatoria(minim 2 columnes i files)");
+                            "1. Crear un taulell buit (introduint les files y columnes)\n" +
+                            "2. Taulell amb malalts de forma aleatoria (minim 2 columnes i files)");
                     byte answerTaulell = scan.nextByte();
                     switch(answerTaulell){
                         case 1:
                             System.out.print("Diga'm les mesures del taulell. Primer les files i després les columnes: ");
                             x = scan.nextInt();
                             y = scan.nextInt();
-                            board = new int[x][y];
+                            board = new float[x][y];
                             files = x;
                             columnes = y;
                             for (int i = 0; i < board.length; i++) {
@@ -44,8 +45,8 @@ public class Pandemia {
                         case  2:
                             x = (int)(Math.random()*9+2);
                             y = (int)(Math.random()*9+2);
-                            System.out.printf("Es creara un taulell amb les següents dimensions(x: %d, y: %d)\n",x, y);
-                            board = new int[x][y];
+                            System.out.printf("Es creara un taulell amb les següents dimensions (x: %d, y: %d)\n",x, y);
+                            board = new float[x][y];
                             files = x;
                             columnes = y;
                             for (int i = 0; i < files; i++) {
@@ -57,6 +58,9 @@ public class Pandemia {
                             for (int i = 0; i < board.length; i++) {
                                 System.out.println(Arrays.toString(board[i]));
                             }
+                            break;
+                        default:
+                            System.out.println("Només es pot introduir un número corresponent a les opcions del menú");
                             break;
                     }
                     // posicions bloquejades es representarana amb null per mostrar una X
@@ -79,10 +83,28 @@ public class Pandemia {
                     }
                     break;
                 case 3:
-
+                    System.out.println("Diga'm la taxa de contagi");
+                    taxaContagi = scan.nextFloat();
+                    for (int i = 0; i < files; i++) {
+                        for (int j = 0; j < columnes; j++) {
+                            board[i][j] += board[i][j] * taxaContagi;
+                        }
+                    }
                     break;
                 case 4:
-
+                    System.out.println("Introdueix de quina forma vols transmetre la teva cura:\n" +
+                            "1. Globalment\n" +
+                            "2. Per a una posició concreta");
+                    byte answerCura = scan.nextByte();
+                    switch (answerCura) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        default:
+                            System.out.println("Només es pot introduir un número corresponent a les opcions del menú");
+                            break;
+                    }
                     break;
                 case 5:
 
